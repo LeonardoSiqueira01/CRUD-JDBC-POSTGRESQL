4e397554e32b04880c37549c619bbd8bab5234d5
Usuário <Usurio@DESKTOP-8MUO8UQ>
2022-11-07 23:15:53 -0300

    Criando conexão com o banco de dados atraves do objeto connection e realizando o teste da conexao com Junit

---
+++ pos_java_jdbc/src/main/java/conexaoJdbc/SingleConnection.java
@@ -0,0 +1,36 @@
+package conexaoJdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class SingleConnection {
+
+	private static String url = "jdbc:postgresql://localhost:5433/posjava";
+	private static String password = "admin";
+	private static String user = "postgres";
+	private static Connection connection = null;
+	
+	static {
+		conectar();
+	}
+	
+	public SingleConnection() {
+		conectar();
+	}
+	
+	private static void conectar () {
+		try {
+			if (connection == null) {
+				Class.forName("org.postgresql.Driver");
+				connection = DriverManager.getConnection(url, user, password);
+				connection.setAutoCommit(false);
+				System.out.println("Coneectou com sucesso!");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+public static Connection getConnection() {
+	 return connection;
+}
+}
---
+++ pos_java_jdbc/src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
@@ -0,0 +1,13 @@
+package pos_java_jdbc.pos_java_jdbc;
+
+import org.junit.Test;
+
+import conexaoJdbc.SingleConnection;
+
+public class TesteBancoJdbc {
+	
+	@Test
+	public void initBanco() {
+		SingleConnection.getConnection();
+	}
+}
